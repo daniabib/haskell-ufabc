e73b4fef10bb31c5345ee5e279b2d096d9ae37ed
Daniel Abib <danielsabib@gmail.com>
2022-06-10 12:07:10 -0300

    End Ch4 - Prog. Haskell

--- ProgrammingHaskell/Ch4/ex02c.hs
+++ ProgrammingHaskell/Ch4/ex02c.hs
@@ -2,4 +2,4 @@ third :: [a] -> a
 third [] = error "List is empty."
 third [x] = error "List has only one element."
 third [_, _] = error "List has only two elements."
-third (_: _: x:_) = x
+third (_ : _ : x : _) = x
---
+++ ProgrammingHaskell/Ch4/ex05.hs
@@ -0,0 +1,8 @@
+(&&) :: Bool -> Bool -> Bool
+(&&) a b =
+  if a == True
+    then
+      if b == True
+        then True
+        else False
+    else False
