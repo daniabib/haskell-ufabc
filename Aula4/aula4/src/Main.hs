55b8f529e2d3c6f7beea5d3ee229b0fe1c8776af
Daniel Abib <danielsabib@gmail.com>
2022-06-26 12:49:06 -0300

    Fim Aula 4.2

--- Aula4/aula4/src/Main.hs
+++ Aula4/aula4/src/Main.hs
@@ -4,14 +4,28 @@ tamanho :: Num p => [a] -> p
 tamanho [] = 0
 tamanho (_ : xs) = 1 + tamanho xs
 
--- Exercício 1
-raizes2grau :: Floating a => a -> a -> a -> (a, a)
+-- Exercício 1, 2 e 3
+raizes2grau :: (Ord a, Floating a) => a -> a -> a -> (a, a)
 raizes2grau a b c =
-  (x1, x2)
+  if delta >= 0
+    then (x1, x2)
+    else (0, 0)
   where
-    x1 = (- b) + sqDelta / (2 * a)
-    x2 = (- b) + sqDelta / (2 * a)
-    sqDelta = sqrt (b ^ 2 - 4 * a * c)
+    x1 = ((- b) + sqDelta) / (2 * a)
+    x2 = ((- b) - sqDelta) / (2 * a)
+    delta = b ^ 2 - 4 * a * c
+    sqDelta = sqrt delta
+
+-- Exercício 4
+raizes2grau2 :: (Ord a, Floating a) => a -> a -> a -> (a, a)
+raizes2grau2 a b c
+  | delta >= 0 = (x1, x2)
+  | otherwise = error "Delta negativo."
+  where
+    x1 = ((- b) + sqDelta) / (2 * a)
+    x2 = ((- b) - sqDelta) / (2 * a)
+    delta = b ^ 2 - 4 * a * c
+    sqDelta = sqrt delta
 
 main :: IO ()
 main = do
